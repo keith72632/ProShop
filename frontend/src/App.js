cb38ac0456f106ac2666edc10698b36eef1539a8
Keith Horton <keithhorton30@outlook.com>
2020-11-19 15:42:32 -0600

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
-function App() {
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+const App = () => {
   return (
-    <header>hello</header>
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+      <h1>Welcome to Proshop</h1>
+      <HomeScreen />
+      </Container>
+     <Footer />
+     </main>
+    </>
   );
 }
 
 export default App;
+
