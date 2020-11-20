f8dd597e7d2f929e1f6893f64125cce1e73cffb6
Keith Horton <keithhorton30@outlook.com>
2020-11-20 15:07:47 -0600

    added routes and dummy data

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app = express();
 import dotenv from 'dotenv'
 import products from './data/products.js'
 import connectDB from './config/db.js'
+import productsRoutes from './routes/productRoutes.js'
 
 connectDB();
 
@@ -11,19 +12,13 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000;
 
+//middleware
+app.use('/api/products', productsRoutes)
+
 app.get('/', (req, res) => {
     res.send('api is running');
 })
 
-
-app.get('/api/products', (req, res) => {
-    res.send(products);
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-    res.status(200).send(product);
-})
 
 app.listen(PORT, () => {
     console.log('Server running in ' + process.env.NODE_ENV + ' on port ' + PORT.yellow.bold)
