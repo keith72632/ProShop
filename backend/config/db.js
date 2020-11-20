ae4c93a68c0027d0e4551e5782bd174bfd2add5d
Keith Horton <keithhorton30@outlook.com>
2020-11-20 14:04:01 -0600

    added models

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const connectDB = async() => {
+    try {
+        const connect = await mongoose.connect("mongodb+srv://thinmint:tigers10@clusterk.98gsi.mongodb.net/proshop?retryWrites=true&w=majority", {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        });
+
+        console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline)
+    } catch(error) {
+        console.error(`Error: ${error.message}`.red.underline.bold)
+        process.exit(1)
+    }
+};
+
+export default connectDB
